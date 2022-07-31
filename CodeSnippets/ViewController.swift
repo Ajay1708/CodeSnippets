c4d2dd6f7ab241c53505eb6579587f4f16d3bec6
Ajay Personal <ajay.nellori@gmail.com>
2022-07-31 14:45:04 +0530

    Worked on Higher Order Functions

--- CodeSnippets/ViewController.swift
+++ CodeSnippets/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        task1()
     }
-
-
+    
+    func task1(){
+        //we want to add the squares of all the even numbers from an array of arrays.
+        let arrayInArray = [[11,12,13],[14,15,16]]
+        var newValue = arrayInArray.flatMap{$0}.filter{$0 % 2 == 0}.map{$0  * $0}.reduce(0, {$0 + $1})
+        print(newValue)
+    }
 }
 
